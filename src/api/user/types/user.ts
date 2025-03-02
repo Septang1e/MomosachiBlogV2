c311c930eeeedd7986c388a9da03bc76974e6891
Momosachi <b2934833295@gmail.com>
2025-03-02 14:04:58 +0800

    chore: backup

---
+++ src/api/user/types/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    pid: string,          // 用户唯一标识
+    username: string,     // 用户名
+    avatar: string,       // 头像URL
+    email?: string,       // 邮箱（可选）
+    website: string
+}
